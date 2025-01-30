47ca267d7229bb6ad07240ca2466c2e628dd4f79
jyotiteena <jingarjyoti13@gmail.com>
2025-01-30 17:26:32 +0530

    changes in sub category

--- controllers/subcategory.controller.js
+++ controllers/subcategory.controller.js
@@ -9,14 +9,3 @@ exports.trash = async (req, res) => {
     const id = req.paras.id;
     await Subcategory.findOneAndDelete(id)
 }
-
-exports.singleSubCat = async (req, res) => {
-    try {
-        
-        const subcategories = await Subcategory.find({ category: req.params.categoryId });
-        console.log(subcategories)
-        res.json(subcategories);
-    } catch (error) {
-        res.status(500).json({ error: "Error fetching subcategories" });
-    }
-}
