61cd4bb34fe98886c81d5260f49fdc7895f4558e
jyotiteena <jingarjyoti13@gmail.com>
2025-02-01 08:53:14 +0530

    route link remove

--- routes/subcategory.route.js
+++ routes/subcategory.route.js
@@ -1,6 +1,5 @@
 const router = require('express').Router()
 const Subcategory = require('../controllers/subcategory.controller')
 router.post('/',Subcategory.store);
-router.get("/:categoryId", Subcategory.singleSubCat);
 
 module.exports = router
